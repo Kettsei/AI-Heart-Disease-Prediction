063a79fa0735e2861c63e0bb0ac6b1762a407f36
Kettsei <110607632+Kettsei@users.noreply.github.com>
2022-10-06 11:51:15 +0700

    Add files via upload

---
+++ aboutUs.java
@@ -0,0 +1,45 @@
+import javax.swing.*;
+import java.awt.*;
+import java.net.*;
+import java.io.*;
+
+
+public class aboutUs extends JPanel {
+	public JEditorPane editor;
+
+	public aboutUs() {
+		this.setLayout(null);
+		setSize(400, 600);
+		setBackground(Color.gray);
+		launch();
+
+	}
+
+	public void launch() {
+		try {
+			URL url = null;
+			try {
+				url = getClass().getResource("about.htm");
+			} catch (Exception e) {
+				System.err.println("Failed to open ");
+				url = null;
+			}
+
+			if (url != null) {
+				editor = new JEditorPane(url);
+				editor.setEditable(false);
+				JScrollPane scroller = new JScrollPane(editor);
+				scroller.setBackground(Color.gray);
+				editor.setBackground(Color.gray);
+				scroller.setBounds(0, 0, 400, 600);
+				add(scroller);
+			}
+
+		} catch (MalformedURLException e) {
+			System.out.println("Malformed URL: " + e);
+		} catch (IOException e) {
+			System.out.println("IOException: " + e);
+		}
+	}
+
+}
